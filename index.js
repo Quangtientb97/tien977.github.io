35d07db0b5259eb93ee9c3d1551e43d97506028e
Quangtien <Quangtientb97@gmail.com>
2020-06-19 21:16:20 +0700

    log user

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ io.sockets.on('connection', function(socket){
 			con.on('error',function(err){
 				console.log('mysql error 78',err.code);
 			});
-			user_unique_id = result[0].unique_id;
+			var user_unique_id = result[0].unique_id;
 		});
 
 		con.query(`CREATE TABLE IF NOT EXISTS user${user_unique_id}_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, device_id INT(10), ThoiGian TIMESTAMP, chieuquay VARCHAR(255), mode INT(10)) ENGINE = InnoDB`, function(err){
